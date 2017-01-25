a18f304e03e7aa45acaa90f470c7b2fcf8b3e5ff
Rajan <walrajan@gmail.com>
2017-01-24 23:57:31 -0500

    new passes

--- hakaru-jit.rkt
+++ hakaru-jit.rkt
@@ -8,7 +8,11 @@
     (lambda (in)
       (read in))))
 
-
+(define internal-ops
+  (apply set '(size index recip nat2prob prob2real + * == && < ||)))
+(define internal-loop-ops
+  (set 'summate 'product 'array))
+;;LEVEL1
 (define (reduce-function exp)
   (define (combine-functions prg [var-type-assoc null])
     (match prg
@@ -18,10 +22,69 @@
   (define-values (args fbody) (combine-functions exp))
   `(function (,@args) ,fbody))
 
-(define (simple? x)
-  (or (symbol? x) (number? x)))
+;;LEVEL2
+(define (simplify-exp exp)
+  (define sa simplify-exp)
+  (match exp
+    [`(function ,args ,body)
+     `(function ,args ,(sa body))]
+    [`(,op ,index from ,start to ,end : ,body)
+     `(,op (,index ,(sa start) ,(sa end)) ,(sa body))]
+    [`(array ,index of ,size : ,body)
+     `(array (,index 0 ,(sa size)) ,(sa body))]
+    [`(match ,tst : true: ,thn false: ,els)
+     `(if ,(sa tst) ,(sa thn) ,(sa els))]
+    [`(,rator ,rands ...)
+     `(,rator ,@(map sa rands))]
+    [else exp]))
+
+;;LEVEL3
+(define (uniquify exp)
+  (define (rv exp vars)
+    (match exp
+      [(? symbol?)
+       (cdr (assoc exp vars))]
+      [(? number?)
+       exp]
+      [`(,rator ,rands ...)
+       `(,rator ,(map (curryr u vars) rands))]))
+  (define (u exp vars)
+    (match exp
+      [`(function ,args ,body)
+       `(function ,args
+                  ,(u body
+                      (map (λ (at) (cons (car at) (car at))) args)))]
+      [`(,op (,index ,start ,end) ,body) #:when (set-member? internal-loop-ops op)
+       (define i (gensym^ index))
+       (define nv (cons (cons index i) vars))
+       `(,op (,i ,(u start vars) ,(u end vars)) ,(u body nv))]
+      [`(if ,tst ,thn ,els)
+       `(if ,(u tst vars) ,(u thn vars) ,(u els vars))]
+      [`(,rator ,rands ...)
+       `(,rator ,@(map (curryr u vars) rands))]
+      [else (rv exp vars)]))
+  (u exp '()))
+
+;;LEVEL4
+(define (simplify-end-iter exp)
+  (define sl simplify-end-iter)
+  (match exp
+    [`(function ,args ,body)
+     `(function ,args ,(sl body))]
+    [`(,op (,index ,start ,end) ,body)
+     #:when (set-member? internal-loop-ops op)
+     (if (or (symbol? end) (number? end) (equal? (car end) 'size))
+         `(,op (,index ,start ,end) ,(sl body))
+         (let ((end^ (gensym^ 'e)))
+           `(let ((,end^ ,(sl end)))
+              (,op (,index ,start ,end^) ,(sl body)))))]
+    [`(,rator ,rands ...)
+     `(,rator ,@(map sl rands))]
+    [else exp]))
 
 (define (reduce-folds body)
+  (define (simple? x)
+    (or (symbol? x) (number? x)))
   (define (wrap-simplify body)
     (if (simple? body)
         (values body identity)
@@ -29,39 +92,24 @@
           (values sym
                   (λ (prg) `(let (,sym ,body) ,prg))))))
   (define rh reduce-folds)
+  (define (init-value op size)
+    (match op
+      ['summate 0]
+      ['product 1]
+      ['array `(new array real ,size)]
+      [else (error "unknown op for init-value")]))
+  (define (get-assign op result index body)
+    (match op
+      ['summate `(assign (+ ,result ,body))]
+      ['product `(assign (* ,result ,body))]
+      ['array `(assign (index ,result ,index) ,body)]))
   (match body
-    [`(summate ,index from ,start to ,end : ,body)
-     (define-values (start-exp sf) (wrap-simplify (rh start)))
-     (define-values (end-exp ef) (wrap-simplify (rh end)))
+    [`(,op (,index ,start ,end) ,body) #:when (set-member? internal-loop-ops op)
      (define result (gensym^ 'r))
-     (sf (ef
-          `(let (,result 0)
-             (begin
-               (loop
-                (,index ,start-exp ,end-exp)
-                (assign ,result (+ ,result ,(rh body))))
-               ,result))))]
-    [`(product ,index from ,start to ,end : ,body)
-     (define-values (start-exp sf) (wrap-simplify (rh start)))
-     (define-values (end-exp ef) (wrap-simplify (rh end)))
-     (define result (gensym^ 'r))
-     (sf (ef
-          `(let (,result 1)
-             (begin
-               (loop
-                (,index ,start-exp ,end-exp)
-                (assign ,result (* ,result ,(rh body))))
-               ,result))))]
-    [`(array ,index of ,size : ,body)
-     (define-values (size-exp sf) (wrap-simplify (rh size)))
-     (define result (gensym^ 'ar))
-     (sf
-      `(let (,result (new array 'real ,size-exp))
-         (begin
-           (loop (,index 0 ,size-exp)
-                 (assign (index ,result ,index) ,(rh body)))
-           ,result)))]
-    [`(match ,tst : true: ,thn false: ,els)
+     `(fold-loop (,index ,start ,end)
+                 (,result ,(init-value op end))
+                 ,(get-assign op result index (rh body)))]
+    [`(if ,tst ,thn ,els)
      `(if ,(rh tst) ,(rh thn) ,(rh els))]
     [`(function ,args ,body)
      `(function ,args ,(rh body))]
@@ -70,14 +118,22 @@
     [else body]))
 
 
-(define internal-ops
-  (apply set '(size index recip nat2prob prob2real + * == && < ||)))
+(define compilers (list reduce-function simplify-exp uniquify simplify-end-iter reduce-folds))
+(define (debug-program prg cmplrs)
+  (pretty-display
+   (for/fold ([prg prg])
+             ([c cmplrs])
+     (pretty-display prg)
+     (printf "applying ~a\n" (object-name c))
+     (c prg))))
 
 
 (module+ test
   (define hello-src (read-file "examples/hello.hkr"))
   (define nbg-src (read-file "examples/naive-bayes-gibbs.hkr"))
-  (display hello-src) (newline)
-  (pretty-display (reduce-folds (reduce-function hello-src)))
-  (pretty-display (reduce-folds (reduce-function nbg-src))))
+  (debug-program hello-src compilers)
+  (debug-program nbg-src compilers)
+  ;; (pretty-display (reduce-folds (simplify-end-iter (uniquify (simplify-exp (reduce-function hello-src))))))
+  ;; (pretty-display (reduce-folds (simplify-end-iter (uniquify (simplify-exp (reduce-function nbg-src))))))
+)
 
