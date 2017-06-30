ce7e7abdd2de74e2e9205d28ed3af0d4572e4c52
Rajan <walrajan@gmail.com>
2017-06-30 12:28:11 -0400

    moving pass-utils to ast

--- private/ast.rkt
+++ private/ast.rkt
@@ -893,3 +893,63 @@
          [(stmt-assign var val)
           (stmt-assign var (f val))]
          [else e])]))
+
+(define (is-complex? expr)
+  (match expr
+    [(expr-sum _ _ _ _ _) #t]
+    [(expr-prd _ _ _ _ _) #t]
+    [(expr-arr _ _ _ _) #t]
+    [(expr-bucket _ _ _ _) #t]
+    [(expr-let _ _ v b) (or (is-complex? v) (is-complex? b))]
+    [(expr-match _ _ brs) (ormap is-complex? brs)]
+    [(expr-branch _ b) (is-complex? b)]
+    [(expr-bind _ b) (is-complex? b)]
+    [(expr-if _ tst thn els) (or (is-complex? tst) (is-complex? thn) (is-complex? els))]
+    [(expr-app _ rt rds)
+     (ormap is-complex? rds)]
+    [(expr-var _ _ _)
+     #f]
+    [(expr-val _ _)
+     #f]))
+
+(define (find-free-variables expr)
+  (define ffv^ find-free-variables)
+  (match expr
+    [(expr-let t s val b) (set-union (ffv^ val) (set-remove (ffv^ b) s))]
+    [(expr-lets ts vars vals b)
+     (set-subtract (apply set-union (cons (ffv^ b) (map ffv^ vals)))
+                   (apply set vars))]
+    [(expr-sum t i start end b)
+     (set-union (ffv^ start) (ffv^ end) (set-remove (ffv^ b) i))]
+    [(expr-prd t i start end b)
+     (set-union (ffv^ start) (ffv^ end) (set-remove (ffv^ b) i))]
+    [(expr-arr t i end b)
+     (set-union (ffv^ end) (set-remove (ffv^ b) i))]
+    [(expr-bucket t s e r)
+     (set-union (ffv^ s) (ffv^ e) (ffv^ r))]
+    [(expr-match t tst brns)
+     (set-union (ffv^ tst) (apply set-union (map ffv^ brns)))]
+    [(expr-branch pat b) (ffv^ b)]
+    [(expr-if t tst thn els) (set-union (ffv^ tst) (ffv^ thn) (ffv^ els))]
+    [(expr-app t rator rands) (apply set-union (map ffv^ rands))]
+    [(expr-block t st bd) (set-union (ffv^ st) (ffv^ bd))]
+    [(expr-val t v) (set)]
+    [(expr-intr sym) (set)]
+    [(expr-intrf sym) (set)]
+    [(expr-var t s o) (set expr)]
+    [(expr-bind s e) (set-remove (ffv^ e) s)]
+
+    [(reducer-split e a b) (set-union (ffv^ e) (ffv^ a) (ffv^ b))]
+    [(reducer-fanout a b) (set-union (ffv^ a) (ffv^ b))]
+    [(reducer-add i) (set-union (ffv^ i))]
+    [(reducer-nop) (set)]
+    [(reducer-index i e b) (set-union (ffv^ i) (ffv^ e) (ffv^ b))]
+
+    [(stmt-if tst thn els) (set-union (ffv^ tst) (ffv^ thn) (ffv^ els))]
+    [(stmt-for i start end body)
+     (set-union (ffv^ start) (ffv^ end) (set-remove (ffv^ body) i))]
+    [(stmt-block stmts)
+     (apply set-union (map ffv^ stmts))]
+
+    [(stmt-void) (set)]
+    [(stmt-assign to val) (set-union (ffv^ to) (ffv^ val))]))
--- private/simplifications.rkt
+++ private/simplifications.rkt
@@ -1,7 +1,8 @@
 #lang racket
 (require "ast.rkt")
 (provide simplify-match
-         simplify-lets)
+         ;; simplify-lets
+         )
 
 (define simplify-match
   (create-rpass
@@ -39,18 +40,18 @@
             (expr-let t cdr-var (expr-app cdr-type (expr-intrf 'cdr) (list tst))
                       (expr-bind-body cdr-bind))))
 
-(define (sl e env)
-  (fill-expr-pass
-   (curryr sl env) e
-   [(expr-let t v val body)
-    (if (expr-var? val)
-        (begin
-          (printf "replacing: ~a with ~a\n" (print-expr v) (print-expr val))
-          (sl body (hash-set env v val)))
-        (expr-let t v (sl val env) (sl body env)))]
-   [v #:when (hash-has-key? env v)
-      (printf "\treplaced: ~a with ~a\n" (print-expr v) (print-expr (hash-ref env v)))
-      (hash-ref env v)]))
+;; (define (sl e env)
+;;   (fill-expr-pass
+;;    (curryr sl env) e
+;;    [(expr-let t v val body)
+;;     (if (expr-var? val)
+;;         (begin
+;;           (printf "replacing: ~a with ~a\n" (print-expr v) (print-expr val))
+;;           (sl body (hash-set env v val)))
+;;         (expr-let t v (sl val env) (sl body env)))]
+;;    [v #:when (hash-has-key? env v)
+;;       (printf "\treplaced: ~a with ~a\n" (print-expr v) (print-expr (hash-ref env v)))
+;;       (hash-ref env v)]))
 
-(define (simplify-lets e)
-  (sl e (make-immutable-hash)))
+;; (define (simplify-lets e)
+;;   (sl e (make-immutable-hash)))
