44d2d8b0785a4cfdb7f7ebc083a8ddd288685c4f
Rajan <walrajan@gmail.com>
2017-07-02 17:54:23 -0400

    simplify lets with map-expr

--- private/simplifications.rkt
+++ private/simplifications.rkt
@@ -1,7 +1,7 @@
 #lang racket
 (require "ast.rkt")
 (provide simplify-match
-         ;; simplify-lets
+         simplify-lets
          )
 
 (define simplify-match
@@ -40,18 +40,20 @@
             (expr-let t cdr-var (expr-app cdr-type (expr-intrf 'cdr) (list tst))
                       (expr-bind-body cdr-bind))))
 
-;; (define (sl e env)
-;;   (fill-expr-pass
-;;    (curryr sl env) e
-;;    [(expr-let t v val body)
-;;     (if (expr-var? val)
-;;         (begin
-;;           (printf "replacing: ~a with ~a\n" (print-expr v) (print-expr val))
-;;           (sl body (hash-set env v val)))
-;;         (expr-let t v (sl val env) (sl body env)))]
-;;    [v #:when (hash-has-key? env v)
-;;       (printf "\treplaced: ~a with ~a\n" (print-expr v) (print-expr (hash-ref env v)))
-;;       (hash-ref env v)]))
+(define (sl e env)
+  (match e
+    [(expr-let t v val body)
+     (if (expr-var? val)
+         (begin
+           (printf "replacing: ~a with ~a\n" (print-expr v) (print-expr val))
+           (sl body (hash-set env v val)))
+         (expr-let t v (sl val env) (sl body env)))]
+    [v #:when (hash-has-key? env v)
+       (printf "\treplaced: ~a with ~a\n" (print-expr v) (print-expr (hash-ref env v)))
+       (hash-ref env v)]
+    [else (define f-expr (curryr sl env))
+          (define f-stmt (λ (e) (map-stmt f-expr identity f-stmt identity e)))
+          (map-expr f-expr identity f-stmt identity e)]))
 
-;; (define (simplify-lets e)
-;;   (sl e (make-immutable-hash)))
+(define (simplify-lets e)
+  (sl e (make-immutable-hash)))
