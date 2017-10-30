7689d65a0bbd4102f89c4078d2c9021035c918a1
Rajan <walrajan@gmail.com>
2017-10-30 14:05:11 -0400

    rator-template function in sham-utils

--- private/sham-utils.rkt
+++ private/sham-utils.rkt
@@ -23,6 +23,11 @@
 (define set-index-fun-format "set-index-array<~a>")
 (define empty-array-fun-format "empty-array<~a>")
 
+(define add-fun-format "add-~a-~a");;add-<num-args>-<type>
+(define mul-fun-format "mul-~a-~a");;mul-<num-args>-<type>
+(define recip-fun-format "recip-~a")
+(define categorical-fun-format "categorical-~a");;categorical-<num-args>
+
 (define type-nat-def (sham:def:type 'nat (sham:type:ref 'i32)))
 (define type-real-def (sham:def:type 'real (sham:type:ref 'f64)))
 (define type-prob-def (sham:def:type 'prob (sham:type:ref 'f64)))
@@ -98,17 +103,21 @@
              (λ () (sham:type:ref (sham:def-id sham-type-def)))))
 (define (get-sham-type-ref-ast type-ast)
   (get-sham-type-ref (get-sham-type-define type-ast)))
+
 ;;ret-type: symbol
 ;;rator: symbol
 ;;rand-types: (list symbol)
-;;returns (cons sham:rator (list sham:def)
-;; (define (get-sham-rator ret-type rator rand-types)
-;;   (match rator
-;;     ['nat2prob]
-;;     ['nat2real]
-;;     ['prob2real]
-;;     ['real2prob]
-;;     ['recip-nat]
-;;     ['recip-real]
-;;     ['recip-prob]))
-  
+;;returns (list format-str args ...)
+(define (get-rator-template ret-type rator rand-types)
+  (match rator
+    ['+ (list add-fun-format (length rand-types) (get-type-sym (car rand-types)))]
+    ['* (list mul-fun-format (length rand-types) (get-type-sym (car rand-types)))]
+    ['recip (list recip-fun-format (get-type-sym (car rand-types)))]
+    ['empty (list make-array-fun-format (get-type-sym ret-type))]
+    ['size (list get-array-size-fun-format (get-type-sym (car rand-types)))]
+    ['index (list get-index-fun-format (get-type-sym (car rand-types)))]
+    ['cons (list make-pair-fun-format (get-type-sym ret-type))]
+    ['car (list pair-car-fun-format (get-type-sym (car rand-types)))]
+    ['cdr (list pair-cdr-fun-format (get-type-sym (car rand-types)))]
+    ['categorical (list categorical-fun-format (length rand-types))]))
+
