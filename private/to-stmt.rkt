f3f9d9182d917457413e09968101533537be6620
Rajan <walrajan@gmail.com>
2017-11-01 00:16:00 -0400

    cleaning up folds->for as it is already done in combine-loops

--- private/to-stmt.rkt
+++ private/to-stmt.rkt
@@ -1,80 +1,9 @@
 #lang racket
 (require "ast.rkt"
          "utils.rkt")
-(provide folds->for
-         to-stmt
+(provide to-stmt
          simplify-set
          remove-if-expr)
-
-(define (folds->for e)
-  (define fns (box '()))
-  (define (add-to-fns fn)
-    (set-box! fns (cons fn (unbox fns))))
-  (define pass
-    (create-rpass
-     (expr [(expr-arr t index size body)
-            (define fbody (do-arr t index size body))
-            (define fvars (set->list (set-remove (find-free-variables fbody) index)))
-            (define fname (gensym^ 'af))
-            (printf "arr: fname ~a, fvars: ~a\n" fname (map pe fvars))
-            (add-to-fns (cons fname (expr-fun fvars t fbody)))
-            (expr-app t (expr-intrf fname) fvars)]
-           [(expr-sum t index start end body)
-            (define fbody (do-sum t index start end body))
-            (define fvars (set->list (set-remove (find-free-variables fbody) index)))
-            (define fname (gensym^ 'sf))
-            (printf "sum: fname ~a, fvars: ~a\n" fname (map pe fvars))
-            (add-to-fns (cons fname (expr-fun fvars t fbody)))
-            (expr-app t (expr-intrf fname) fvars)]
-           [(expr-prd t index start end body)
-            (define fname (gensym^ 'pf))
-            (define fbody (do-prd t index start end body))
-            (define fvars (set->list (set-remove (find-free-variables fbody) index)))
-            (printf "prd: fname ~a, fvars: ~a\n" fname (map pe fvars))
-            (define fn (expr-fun fvars t fbody))
-            (add-to-fns (cons fname fn))
-            (expr-app t (expr-intrf fname) fvars)])
-     (reducer)
-     (stmt)
-     (pat)))
-  (define new-mod (pass e))
-  (expr-mod (expr-mod-main new-mod) (append (expr-mod-fns new-mod) (unbox fns))))
-
-(define (do-arr t index size body)
-  (make-fold t
-             (expr-var t (gensym^ 'ar) '_)
-             (expr-app t (expr-intrf (symbol-append 'empty- (get-print-type t)))
-                       (list size))
-             index (expr-val 'nat 0) size
-             (λ (index result) (stmt-assign (expr-app t (expr-intrf 'index)
-                                                      (list result index)) body))))
-
-(define (do-sum t index start end body)
-  (make-fold t
-             (expr-var t (gensym^ 'sr) '_)
-             (expr-val t 0)
-             index start end
-             (λ (index result) (stmt-assign result (expr-app t (expr-intrf '+)
-                                                             (list result body))))))
-
-(define (do-prd t index start end body)
-  (make-fold
-   t
-   (expr-var t (gensym^ 'pr) '_)
-   (expr-val t 1)
-   index start end
-   (λ (index result)
-     (expr->stmt body (λ (e)
-                        (stmt-assign result (expr-app t (expr-intrf '*)
-                                                      (list result e))))))))
-
-;; create a fold expression with the function body-gen being called with index and result
-(define (make-fold t result init-value index start end body-gen)
-  (expr-let t result init-value
-            (expr-block t
-                        (stmt-for index start end
-                                  (body-gen index result))
-                        result)))
 
 (define (mod-stmt m)
   (match m
