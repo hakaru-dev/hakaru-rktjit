4c9da8b4693789de8b8a7be43307e44b12d83784
Rajan <walrajan@gmail.com>
2017-11-06 13:23:36 -0500

    simpler require for prelude

--- private/prelude.rkt
+++ private/prelude.rkt
@@ -1,22 +1,4 @@
 #lang racket
 
-(require sham/ast)
-
-(provide (all-defined-out))
-;;TODO make this assoc
-(define (new-prelude)
-  (box '()))
-
-(define (add-defs-prelude! prelude defs)
-  (set-box! prelude (append (unbox prelude) defs))
-  prelude)
-
-(define (get-defs-prelude prelude)
-  (unbox prelude))
-
-(define (cleanup-defs defs)
-  (define already-seen (mutable-set))
-  (for/list ([def defs]
-             #:when (not (set-member? already-seen (sham:def-id def))))
-    (set-add! already-seen (sham:def-id def))
-    def))
+(require "prelude/defs.rkt")
+(provide (all-from-out "prelude/defs.rkt"))
