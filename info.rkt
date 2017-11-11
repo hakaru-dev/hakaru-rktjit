52faff3287ca68bf2ed8e3d84569ca3fb9b4ff96
Chung-chieh Shan <ccshan@indiana.edu>
2017-11-10 23:22:50 -0500

    Depend on the "disassemble" package

--- info.rkt
+++ info.rkt
@@ -2,6 +2,7 @@
 
 (define collection "hakrit")
 (define deps '("base"
+               "disassemble"
                "rackunit-lib"))
 (define pkg-desc "A jit for hakaru simplified output")
 (define version "0.1")
