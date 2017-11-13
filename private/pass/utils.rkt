f0eb8d60b9203d23808d237e9e89ec5d23924e2d
Rajan <walrajan@gmail.com>
2017-11-13 18:34:40 -0500

    pass utils with state for pipeline

--- private/pass/utils.rkt
+++ private/pass/utils.rkt
@@ -8,17 +8,16 @@
          gensym^
          (all-from-out "../utils.rkt"))
 
-;; converts an expression to statment with the value given to
+;; converts an expression to a simple statment with the value given to
 ;; assign-to function at the end
 (define (expr->stmt e assign-to)
   (define (ers e)
     (match e
       [(expr-if typ tst thn els)
        (stmt-if tst (ers thn) (ers els))]
-      ;; [(expr-let t var val body)
-      ;;  (stmt-elets (list var) (list val) (ers body))]
       [(expr-lets types vars vals s body)
-       (stmt-expr (stmt-void) (expr-lets types vars vals (stmt-block (list s (ers body))) (expr-val 'nat 0)))]
+       (stmt-expr (stmt-void)
+                  (expr-lets types vars vals (stmt-block (list s (ers body))) (expr-val 'nat 0)))]
       [else (assign-to e)]))
   (ers e))
 
@@ -26,6 +25,23 @@
   (define (gensym^ sym [sep ""])
     (define n (add1 (hash-ref gensym-hash sym 0)))
     (hash-set! gensym-hash sym n)
-    (string->symbol (string-append (symbol->string sym)
-                                   sep
-                                   (number->string n)))))
+    (string->symbol (string-append (symbol->string sym) sep (number->string n)))))
+
+(struct state (prg info rest-pass))
+(define (run-next new-prg new-info old-state)
+  (match old-state
+    [(state _ _ '())
+     (values new-prg new-info)]
+    [(state _ _ rest-pass)
+     ((car rest-pass) (state new-prg new-info (cdr rest-pass)))]))
+
+(define (run-state st)
+  (match st
+    [(state prg info passes)
+     ((car passes) (state prg info (cdr passes)))]))
+
+(define (vector-getpos vec f?)
+  (map cadr (filter (λ (vi) (f? (car vi)))  (map list (vector->list vec) (build-list (vector-length vec) identity)))))
+
+;;info syms
+(define prog-arg-info 'prog-arg-info)
