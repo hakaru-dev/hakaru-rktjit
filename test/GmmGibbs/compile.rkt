ff92c4104bbe2fb0f43d91c6ab5201a0ea37e5d3
rajan <wal.rajan@gmail.com>
2019-01-05 17:42:16 -0500

    gmmgibbs tests

---
+++ test/GmmGibbs/compile.rkt
@@ -0,0 +1,54 @@
+#lang racket
+
+(require sham
+         sham/jit-utils
+         hakrit/jit
+         hakrit/utils)
+(require ffi/unsafe)
+(define fname "input2.hkr")
+(define input-dir "./1half")
+
+(define input '([-3.8907259534842202
+                 -6.618193122202014
+                 -4.941794905899988
+                 -7.119531519149578
+                 -5.9345748166212
+                 4.825733087265147
+                 -8.331891801596736
+                 -5.597647903988242
+                 -9.082321592213654
+                 14.219895056304555]
+                .
+                [4 4 4 4 4 7 0 4 0 6]))
+(define classes 9)
+(define points 10)
+
+(define stdev (real->prob 14.0))
+(define zs (make-sized-hakrit-array (cdr input) 'nat))
+(define as (make-sized-hakrit-array (list 0.0 0.0 0.0
+                                          0.0 0.0 0.0
+                                          0.0 0.0 0.0) 'real))
+(define t (make-sized-hakrit-array (car input) 'real))
+(define doc 0)
+
+(define module-env (debug-file (build-path input-dir fname) '()))
+(begin
+  (define (gf id) (get-function module-env id))
+  (define array-make (gf 'array-make))
+  (define array-clear (gf 'array-clear))
+  (define array-free (gf 'array-free))
+  (define array-get-size (gf 'array-get-size))
+  (define array-ref (gf 'array-ref))
+  (define array-set! (gf 'array-set!)))
+
+(define prog (get-prog module-env))
+
+(define result (prog stdev as zs t doc))
+(pretty-print (sized-hakrit-array->racket-list result 'prob))
+
+
+(define nocat
+  '(268.0596789212606 272.15138815387155 272.15138815387155 272.15138815387155 274.6138467723381 272.15138815387155 193.6259482527394 256.24364009217334 272.15138815387155))
+(define 1half
+  '(-10.818090761678354 -14.355996628824293 -14.355996628824293 -14.355996628824293 -10.013711119074703 -14.355996628824293 -11.36655052546099 -11.36655052546099 -14.355996628824293))
+(define 2half '(278.877769682939 286.50738478269585 286.50738478269585 286.50738478269585 284.6275578914128 286.50738478269585 204.99249877820037 267.6101906176343 286.50738478269585))
---
+++ test/GmmGibbs/hs/1half/Main2.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE CPP, DataKinds, NegativeLiterals #-}
+module Main where
+
+import           Data.Number.LogFloat (LogFloat)
+import           Prelude hiding (product, exp, log, (**))
+import           Language.Hakaru.Runtime.LogFloatPrelude
+
+import           Language.Hakaru.Runtime.CmdLine
+import           Language.Hakaru.Types.Sing
+import qualified System.Random.MWC                as MWC
+import           Control.Monad
+import           System.Environment (getArgs)
+
+import qualified Data.Time.Clock as C
+import qualified System.Environment as SE
+import qualified Data.Number.LogFloat as LF
+import qualified Data.Vector.Unboxed as UV
+import qualified Data.Text.IO as TIO
+import qualified Data.Text as T
+import qualified Data.Text.Read as TR
+
+prog ::
+  (Prob ->
+   ((MayBoxVec Prob Prob) ->
+    ((MayBoxVec Int Int) ->
+     ((MayBoxVec Double Double) -> (Int -> (MayBoxVec Double Double))))))
+
+prog =
+  lam $ \ s44 ->
+  lam $ \ as45 ->
+  lam $ \ z46 ->
+  lam $ \ t47 ->
+  lam $ \ docUpdate48 ->
+          (array (size as45) $
+                                           \ zNewd60 ->
+                                             (fromInt (let_ (bucket (nat_ 0)
+                                                                              (size t47)
+                                                                              ((r_index (\ () ->
+                                                                                         size as45)
+                                                                                        (\ (_a62,()) ->
+                                                                                         z46
+                                                                                         ! _a62)
+                                                                                        (r_add (\ (_a62,(zNewd63,())) ->
+                                                                                                nat_ 1))))) $ \ summary61 ->
+                                                                 nat2int (case_ (zNewd60
+                                                                                 == z46
+                                                                                    ! docUpdate48)
+                                                                                [branch ptrue
+                                                                                        (nat_ 1),
+                                                                                 branch pfalse
+                                                                                        (nat_ 0)]) *
+                                                                 int_ -1 +
+                                                                 nat2int (summary61 ! zNewd60)) +
+                                                        fromProb (as45 ! zNewd60)))
+
+
+main :: IO ()
+main = do
+  let points = 10
+      classes = 9
+      stddev = 14
+      as = array classes (const 1)
+      ts = UV.fromList ([-3.8907259534842202,-6.618193122202014,-4.941794905899988,-7.119531519149578,-5.9345748166212,4.825733087265147,-8.331891801596736,-5.597647903988242,-9.082321592213654,14.219895056304555] :: [Double])
+      zs = UV.fromList [4,4,4,4,4,7,0,4,0,6]
+      doc = 0
+  let result = (prog stddev as zs ts doc)
+  print result
