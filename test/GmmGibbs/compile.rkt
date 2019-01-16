058c485ca71472f618c9b8b3dc754ed203b50c97
rajan <wal.rajan@gmail.com>
2019-01-16 11:20:56 -0500

    making gmmGibbs tests with rackunit

--- test/GmmGibbs/compile.rkt
+++ test/GmmGibbs/compile.rkt
@@ -31,46 +31,34 @@
 (define t (make-sized-hakrit-array (car input) 'real))
 (define doc 0)
 
-(define module-env (debug-file (build-path input-dir fname) '()))
-(begin
-  (define (gf id) (get-function module-env id))
-  (define array-make (gf 'array-make))
-  (define array-clear (gf 'array-clear))
-  (define array-free (gf 'array-free))
-  (define array-size (gf 'array-get-size))
-  (define array-ref (gf 'array-ref))
-  (define array-set! (gf 'array-set!)))
+(define (run-test input-dir fname)
+  (define module-env (debug-file (build-path input-dir fname) '()))
+  (begin
+    (define (gf id) (get-function module-env id))
+    (define array-make (gf 'array-make))
+    (define array-clear (gf 'array-clear))
+    (define array-free (gf 'array-free))
+    (define array-size (gf 'array-get-size))
+    (define array-ref (gf 'array-ref))
+    (define array-set! (gf 'array-set!)))
 
-(define prog (get-prog module-env))
+  (define prog (get-prog module-env))
 
-(define result (prog stdev as zs t doc))
-(pretty-print (sized-hakrit-array->racket-list result 'prob))
+  (define result (prog stdev as zs t doc))
+  (sized-hakrit-array->racket-list result 'prob))
 
+(module+ test
+  (require rackunit)
+  (define nocat-result (run-test "./nocat" "input.hkr"))
+  (define nocat-expected
+    '(268.0596789212606 272.15138815387155 272.15138815387155 272.15138815387155 274.6138467723381 272.15138815387155 193.6259482527394 256.24364009217334 272.15138815387155))
+  (map (λ (v1 v2) (check-= v1 v2 0.00001 "GmmGibbs.nocat")) nocat-result nocat-expected )
 
-(define nocat
-  '(268.0596789212606 272.15138815387155 272.15138815387155 272.15138815387155 274.6138467723381 272.15138815387155 193.6259482527394 256.24364009217334 272.15138815387155))
-(define 1half
-  '(-10.818090761678354 -14.355996628824293 -14.355996628824293 -14.355996628824293 -10.013711119074703 -14.355996628824293 -11.36655052546099 -11.36655052546099 -14.355996628824293))
-(define 2half '(278.877769682939 286.50738478269585 286.50738478269585 286.50738478269585 284.6275578914128 286.50738478269585 204.99249877820037 267.6101906176343 286.50738478269585))
+  (define 1half-result (run-test "./1half" "input.hkr"))
+  (define 1half-expected
+    '(-10.818090761678354 -14.355996628824293 -14.355996628824293 -14.355996628824293 -10.013711119074703 -14.355996628824293 -11.36655052546099 -11.36655052546099 -14.355996628824293))
+  (map (λ (v1 v2) (check-= v1 v2 0.00001 "GmmGibbs.1half")) 1half-result 1half-expected )
 
-
-
-;; bucket manual
-;; (define bk (make-sized-hakrit-array (build-list 10 (const 0)) 'nat))
-;; (for ([i (range 10)]) (array-set! bk (array-ref zs i) (+ (array-ref bk (array-ref zs i)) 1)))
-
-
-;; (define ta (make-sized-hakrit-array (build-list 10 (const 5.0)) 'real))
-;; (sized-hakrit-array->racket-list ta 'real)
-;; ;; '(5.0 5.0 5.0 5.0 5.0 5.0 5.0 5.0 5.0 5.0)
-;; (array-clear ta)
-;; (sized-hakrit-array->racket-list ta 'real)
-;; ;; '(0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 5.0)
-
-
-;; (define tb (make-sized-hakrit-array (build-list 10 (const 5)) 'nat))
-;; (sized-hakrit-array->racket-list tb 'nat)
-;; ;; '(5 5 5 5 5 5 5 5 5 5)
-;; (array-clear tb)
-;; (sized-hakrit-array->racket-list tb 'nat)
-;; ;; '(0 0 0 0 0 0 0 0 0 0)
+  (define 2half-result (run-test "./2half" "input.hkr"))
+  (define 2half-expected '(278.877769682939 286.50738478269585 286.50738478269585 286.50738478269585 284.6275578914128 286.50738478269585 204.99249877820037 267.6101906176343 286.50738478269585))
+  (map (λ (v1 v2) (check-= v1 v2 0.00001 "GmmGibbs.2half")) 2half-result 2half-expected))
