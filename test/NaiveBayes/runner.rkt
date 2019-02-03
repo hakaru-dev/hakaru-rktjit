67f5e8577c9086cffb153757744beda6364f4a2b
rajan <wal.rajan@gmail.com>
2019-02-03 12:40:15 -0500

    tests

--- test/NaiveBayes/runner.rkt
+++ test/NaiveBayes/runner.rkt
@@ -1,56 +1,39 @@
 #lang racket
-(require ffi/unsafe
+(require sham
+         sham/jit-utils
+         hakrit/jit
+         hakrit/utils
          rackunit
          racket/runtime-path)
 
-(require "../../jit.rkt"
-         sham
-         "utils.rkt"
-         "../../utils.rkt")
+(define module-env (compile-file "./NaiveBayesGibbs.hkr" '()))
+(define prog (get-prog module-env))
 
-(define vs-topics (list 2 1 0 2 1 0 0))
-(define vs-words (list 0 3 3 2 1 2 0 1 2 3 3 0 0 3 2 1 0))
-(define vs-docs (list 0 0 1 1 1 2 2 2 3 4 4 5 5 5 6 6 6))
+(define news-dir "./news/")
+(define words (map string->number (file->lines (build-path news-dir "words"))))
+(define docs (map string->number (file->lines (build-path news-dir "docs"))))
+(define topics (map string->number (file->lines (build-path news-dir "topics"))))
 
-(define full-words (map string->number (file->lines "./news/words")))
-(define full-docs (map string->number (file->lines "./news/docs")))
-(define full-topics (map string->number (file->lines "./news/topics")))
+(define words-size (length words))
+(define docs-size (length docs))
+(define topics-size (length topics))
 
-(define empty-nbinfo (list '() '() '() '() '() '()))
+(define num-docs (add1 (last docs)))
+(define num-words (add1 (argmax identity words)))
+(define num-topics (add1 (argmax identity topics)))
 
-(define (create-test module-env topics words docs output-type)
-  (define prog (jit-get-function 'prog module-env))
-  (define init-rng (jit-get-function 'init-rng module-env))
-  (init-rng)
-  (printf "compiled\n")
-  (define num-topics (add1 (argmax identity topics)))
-  (define num-words (add1 (argmax identity words)))
-  (define num-docs (add1 (last docs)))
+(define topic-prior (make-sized-hakrit-array (build-list num-topics (const 0.0)) 'real))
+(define word-prior (make-sized-hakrit-array (build-list num-words (const 0.0)) 'real))
 
-  (define topic-prior
-    (rkt->jit module-env '(array prob) (build-list num-topics (const 1.0))))
-  (define word-prior
-    (rkt->jit module-env '(array prob) (build-list num-words (const 1.0))))
+(define c-words (make-sized-hakrit-array words 'nat))
+(define c-docs (make-sized-hakrit-array docs 'nat))
+(define zsc (make-sized-hakrit-array topics 'nat))
 
-  (define c-words (rkt->jit module-env '(array nat) words))
-  (define c-docs (rkt->jit module-env '(array nat) docs))
-  (define zs (rkt->jit module-env '(array nat) topics))
-  (lambda (doc)
-    (printf "calling prog\n")
-    (define init-time (get-time))
-    (define output-c (prog topic-prior word-prior zs c-words c-docs doc))
-    (define el-time (elasp-time init-time))
-
-    (printf "output from c: ~a\n" output-c)
-    (define jit-out (jit->rkt module-env output-type output-c))
-    (printf "time taken: ~a\n" el-time)
-    (printf "output: ~a\n" jit-out)
-    output-c))
-
-(define penv (compile-file "./NaiveBayesGibbs.hkr" empty-nbinfo))
-(define run-full-test
-  (create-test penv full-topics full-words full-docs 'nat))
 
 (module+ test
-  (define our-out (run-full-test 19800))
-  (printf "output: ~a\n" our-out))
+  (require rackunit)
+  (define result (prog topic-prior word-prior zsc c-words c-docs 99))
+  (for/list ([i (in-range 10)])
+    (prog topic-prior word-prior zsc c-words c-docs 99))
+  (printf "result: ~a\n" result)
+  )
---
+++ test/simple/runner.rkt
@@ -0,0 +1,79 @@
+#lang racket
+
+(require sham
+         sham/jit-utils
+         hakrit/jit
+         hakrit/utils)
+
+(define fname "array-product.hkr")
+(define input-dir "./")
+(define input-array-real (list 1.0 2.0 3.0 4.0 5.0))
+(define input-array (map real->prob input-array-real))
+(define sized-input-array (make-sized-hakrit-array input-array 'real))
+(define fixed-input-array (make-fixed-hakrit-array input-array 'real))
+
+(define (compile-and-run fname inputs (info '()))
+  (printf "compiling: ~a with info: ~a\n" fname info)
+  (define module-env (debug-file (build-path input-dir fname) info))
+  (define prog (get-prog module-env))
+  (apply prog inputs))
+
+(module+ test
+  (require rackunit)
+  (define ags "array-get-size.hkr")
+  (define arr "array.hkr")
+  (define arp "array-product.hkr")
+  (define ars  "array-summate.hkr")
+
+  #;(begin
+    (printf "normals\n")
+    (begin
+      (printf "normal array-get-size\n")
+      (define result (compile-and-run ags (list sized-input-array)))
+      (check-equal? result (length input-array)))
+
+    (begin
+      (printf "normal array\n")
+      (define result
+        (sized-hakrit-array->racket-list (compile-and-run arr (list sized-input-array)) 'prob))
+      (map (λ (v1 v2) (check-= v1 v2 0.00001)) result input-array)
+      (map (λ (v1 v2) (check-= (prob->real v1) (prob->real v2) 0.00001)) result input-array))
+
+    (begin
+      (printf "normal array-product\n")
+      (define result (compile-and-run arp (list sized-input-array)))
+      (check-equal? result (foldr + 0 input-array)))
+
+    (begin
+      (printf "normal array-summate\n")
+      (define result (compile-and-run ars (list sized-input-array)))
+      (check-equal? (prob->real result) (foldr + 0 input-array-real))))
+
+  (begin
+    (printf "optimized\n")
+    (define info `((z . ((array-info . ((size . ,(length input-array))))))))
+
+    (begin
+      (printf "optimized array-get-size\n")
+      (define result-ags (compile-and-run ags (list sized-input-array) info))
+      (check-equal? result-ags (length input-array)))
+
+    (begin
+      (printf "optimized array\n")
+      (define raw-result-arr  (compile-and-run arr (list sized-input-array) info))
+      (define result-arr (sized-hakrit-array->racket-list raw-result-arr 'prob))
+      (map (λ (v1 v2) (check-= v1 v2 0.00001)) result-arr input-array)
+      (map (λ (v1 v2) (check-= (prob->real v1) (prob->real v2) 0.00001)) result-arr input-array))
+
+    ;; (begin
+    ;;   (printf "optimized array-product\n")
+    ;;   (define result (compile-and-run arp (list sized-input-array)))
+    ;;   (check-equal? result (foldr + 0 input-array)))
+
+    ;; (begin
+    ;;   (printf "optimized array-summate\n")
+    ;;   (define result (compile-and-run ars (list sized-input-array)))
+    ;;   (check-equal? (prob->real result) (foldr + 0 input-array-real)))
+    )
+
+  )
